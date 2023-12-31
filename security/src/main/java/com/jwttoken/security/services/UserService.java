f98600be7f9f508bf49fe34a41bdd64ac603e21c
ManishDevlikar <manish.devlikar5003@gmail.com>
2023-12-31 18:43:39 +0530

    jwt token based authentication added

--- security/src/main/java/com/jwttoken/security/services/UserService.java
+++ security/src/main/java/com/jwttoken/security/services/UserService.java
@@ -14,10 +14,10 @@ public class UserService {
 	List<User> userList = new ArrayList<User>();
 
 	public UserService() {
-		userList.add(new User(UUID.randomUUID().toString(), "manish", 70.0));
-		userList.add(new User(UUID.randomUUID().toString(), "sunny", 80.0));
-		userList.add(new User(UUID.randomUUID().toString(), "raj", 75.0));
-		userList.add(new User(UUID.randomUUID().toString(), "kushank", 85.0));
+		userList.add(new User(UUID.randomUUID().toString(), "manish", 70.0, "manishdevlikar@gmail.com"));
+		userList.add(new User(UUID.randomUUID().toString(), "sunny", 80.0, "manishdevlikar@gmail.com"));
+		userList.add(new User(UUID.randomUUID().toString(), "raj", 75.0, "manishdevlikar@gmail.com"));
+		userList.add(new User(UUID.randomUUID().toString(), "kushank", 85.0, "manishdevlikar@gmail.com"));
 	}
 
 	public List<User> getUsers() {
